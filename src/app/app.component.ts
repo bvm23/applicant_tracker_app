801b7922be69e25593872029af4680df348fe4da
bvm23 <bezalelvm17@gmail.com>
2025-03-25 03:00:07 +0530

    added header in homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [HeaderComponent],
 })
 export class AppComponent {
   title = 'applicant_tracker';
