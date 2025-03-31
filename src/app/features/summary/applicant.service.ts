e51bfc967b314eb2bea809d7196978c75c3ff496
bvm23 <bezalelvm17@gmail.com>
2025-03-31 19:23:14 +0530

    duplicate-user option and functionality added in menu dropdown component

--- src/app/features/summary/applicant.service.ts
+++ src/app/features/summary/applicant.service.ts
@@ -13,4 +13,18 @@ export class ApplicantService {
   }
 
   allApplicants = this.applicants.asReadonly();
+
+  addApplicant(user: Applicant, isDuplicate: boolean = false) {
+    let duplicateUser: Applicant;
+    if (isDuplicate) {
+      duplicateUser = { ...user };
+      let newUserId = Math.random().toString();
+      duplicateUser.id = newUserId;
+    }
+    this.applicants.update((existing) => [
+      ...existing,
+      isDuplicate ? duplicateUser : user,
+    ]);
+    console.log(this.applicants());
+  }
 }
--- src/app/shared/components/popup-menu/popup-menu.component.ts
+++ src/app/shared/components/popup-menu/popup-menu.component.ts
@@ -1,12 +1,25 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Applicant } from '../../../features/summary/applicant.model';
+import { ActionButtonComponent } from '../action-button/action-button.component';
+import { Copy, Link, CornerUpRight, Trash2 } from 'lucide-angular';
+import { ApplicantService } from '../../../features/summary/applicant.service';
 
 @Component({
   selector: 'at-popup-menu',
-  imports: [],
+  imports: [ActionButtonComponent],
   templateUrl: './popup-menu.component.html',
   styleUrl: './popup-menu.component.scss',
 })
 export class PopupMenuComponent {
+  private apService = inject(ApplicantService);
   user = input.required<Applicant>();
+  linkIcon = Link;
+  duplicateIcon = Copy;
+  moveIcon = CornerUpRight;
+  deleteIcon = Trash2;
+
+  onDuplicate() {
+    console.log('duplicate');
+    this.apService.addApplicant(this.user(), true);
+  }
 }
