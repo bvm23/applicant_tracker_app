d6ff14a1297d89dbf434301530a7d6f326be2230
bvm23 <bezalelvm17@gmail.com>
2025-05-07 22:54:37 +0530

    build error fix 3

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCNMkf0WNYQRw4cYv7CcygtHBBvBkPtXGw',
+    authDomain: 'applicant-tracker-40c59.firebaseapp.com',
+    projectId: 'applicant-tracker-40c59',
+    storageBucket: 'applicant-tracker-40c59.firebasestorage.app',
+    messagingSenderId: '849357409647',
+    appId: '1:849357409647:web:75a7d02de4ef250ac0ac9a',
+  },
+};
