f1684dad5e2fd91da230ec2042199027bc23178c
bvm23 <bezalelvm17@gmail.com>
2025-05-10 22:30:30 +0530

    build error fix using .env

---
+++ envPopulate.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+const successColor = "\x1b[32m%s\x1b[0m";
+const checkSign = "\u{2705}";
+const dotenv = require("dotenv").config({ path: "src/.env" });
+
+const envFile = `export const environment = {
+    firebaseConfig: {
+      apiKey: '${process.env.API_KEY}',
+      authDomain: '${process.env.AUTH_DOMAIN}',
+      projectId: '${process.env.PROJECT_ID}',
+      storageBucket: '${process.env.STORAGE_BUCKET}',
+      messagingSenderId: '${process.env.MESSAGING_SENDER_ID}',
+      appId: '${process.env.APP_ID}',
+  },
+};
+`;
+
+const targetPath = path.join(__dirname, "./src/environments/environment.ts");
+fs.writeFile(targetPath, envFile, (err) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  } else {
+    console.log(
+      successColor,
+      `${checkSign} Successfully generated environment.ts`
+    );
+  }
+});
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,10 @@
-import { FirebaseOptions } from 'firebase/app';
-
 export const environment = {
-  firebaseConfig: process.env['FIREBASE_CONFIG'] as FirebaseOptions,
+  firebaseConfig: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+  },
 };
